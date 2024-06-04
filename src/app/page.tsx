e0b1a915e373180dd967f7053465b42817f5e0a7
Beau McCartney <mccartney.beausl@gmail.com>
2024-06-04 17:39:26 -0600

    dumb deduplicate stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,28 +3,43 @@ import Link from "next/link";
 export default function HomePage() {
   const email = "mccartney.beausl@gmail.com";
 
+  const toplinks: [string, string][] = [
+    ["https://github.com/BeauSLM", "GitHub"],
+    ["https://www.linkedin.com/in/beau-mccartney-247b0820a/", "LinkedIn"],
+    ["beaumccartney-resume.pdf", "Resume"],
+  ];
+
+  const projects: [string, string, string][] = [
+    [
+      "https://crp-docs.vercel.app/",
+      "create-ros-project",
+      "A simple and fast CLI tool for managing ROS (Robotics Operating System) projects.",
+    ],
+
+    [
+      "https://github.com/beauslm/golf",
+      "golf - [gol]ang [f]ragment",
+      "Compile a subset of golang to mips-32 assembly. Handwritten tokenizer, recursive descent parser, semantic checker, and assembly generator.",
+    ],
+
+    [
+      "https://github.com/SamuelGarm/CPSC-585",
+      "maximus overdrive",
+      "A 3d roman chariot racing game built in OpenGL and C++.",
+    ],
+  ];
+
   return (
     <main className="flex min-h-screen flex-col gap-5">
       <header>
         <ul className="flex flex-row flex-wrap gap-10 text-lg">
-          <li className="hover:underline">
-            <Link href="https://github.com/BeauSLM" target="_blank">
-              GitHub
-            </Link>
-          </li>
-          <li className="hover:underline">
-            <Link
-              href="https://www.linkedin.com/in/beau-mccartney-247b0820a/"
-              target="_blank"
-            >
-              LinkedIn
-            </Link>
-          </li>
-          <li className="order-last ml-auto hover:underline">
-            <Link href="beaumccartney-resume.pdf" target="_blank">
-              Resume
-            </Link>
-          </li>
+          {toplinks.map(([link, str]) => (
+            <li className="last:ml-auto hover:underline" key={str}>
+              <Link href={link} target="_blank">
+                {str}
+              </Link>
+            </li>
+          ))}
         </ul>
       </header>
       <section>
@@ -55,39 +70,19 @@ export default function HomePage() {
           Projects
         </h2>
         <ul className="flex flex-col gap-3">
-          <li className="rounded-md border border-neutral-200 py-4 pl-3 pr-10 hover:bg-neutral-200">
-            <Link href="https://crp-docs.vercel.app/" target="_blank">
-              <h3 className="mb-2 text-lg font-semibold text-neutral-800">
-                create-ros-project
-              </h3>
-              <p className="text-neutral-500">
-                A simple and fast CLI tool for managing ROS (Robotics Operating
-                System) projects.
-              </p>
-            </Link>
-          </li>
-          <li className="rounded-md border border-neutral-200 py-4 pl-3 pr-10 hover:bg-neutral-200">
-            <Link href="https://github.com/beauslm/golf" target="_blank">
-              <h3 className="mb-2 text-lg font-semibold text-neutral-800">
-                golf - [gol]ang [f]ragment
-              </h3>
-              <p className="text-neutral-500">
-                Compile a subset of golang to mips-32 assembly. Handwritten
-                tokenizer, recursive descent parser, semantic checker, and
-                assembly generator.
-              </p>
-            </Link>
-          </li>
-          <li className="rounded-md border border-neutral-200 px-2 py-4 hover:bg-neutral-200">
-            <Link href="https://github.com/SamuelGarm/CPSC-585" target="_blank">
-              <h3 className="mb-2 text-lg font-semibold text-neutral-800">
-                maximus overdrive
-              </h3>
-              <p className="text-neutral-500">
-                A 3d roman chariot racing game built in OpenGL and C++.
-              </p>
-            </Link>
-          </li>
+          {projects.map(([link, name, desc]) => (
+            <li
+              className="rounded-md border border-neutral-200 py-4 pl-3 pr-10 hover:bg-neutral-200"
+              key={name}
+            >
+              <Link href={link} target="_blank">
+                <h3 className="mb-2 text-lg font-semibold text-neutral-800">
+                  {name}
+                </h3>
+                <p className="text-neutral-500">{desc}</p>
+              </Link>
+            </li>
+          ))}
         </ul>
       </section>
     </main>
