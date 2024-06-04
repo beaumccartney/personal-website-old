8633e882539ffc5fd418c5b06c8dbc9d6fd3da86
Beau McCartney <mccartney.beausl@gmail.com>
2024-06-03 19:54:20 -0600

    link to projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,11 +1,25 @@
+import Link from 'next/link';
+
 export default function Projects() {
   return (
     <>
-      TODO
-      - golf
-      - maximus overdrive
-      - crp
-      - bear?
+      <ul>
+        <li>
+          <Link href="https://crp-docs.vercel.app/" target="_blank">
+            create-ros-project
+          </Link>
+        </li>
+        <li>
+          <Link href="https://github.com/beauslm/golf" target="_blank">
+            golf
+          </Link>
+        </li>
+        <li>
+          <Link href="https://github.com/SamuelGarm/CPSC-585" target="_blank">
+            maximus overdrive
+          </Link>
+        </li>
+      </ul>
     </>
   );
 }
