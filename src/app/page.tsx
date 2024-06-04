7fd5d9eb4570ea28ac83fdddadc644128c1e48fd
Beau McCartney <mccartney.beausl@gmail.com>
2024-06-03 20:25:28 -0600

    "professional" and "not professional" paragraphs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,23 +5,33 @@ export default function HomePage() {
     <>
       <main>
         <p>
-          Hello, I&apos;m Beau! I&apos;m a software engineer interested in
-          compilers, game engine technology, and robotics.
+          Hello, I&apos;m Beau! I love learning about the world around me, and
+          exploring it by building.
+        </p>
+        <br />
+        <p>
+          I studied software engineering at the University of Calgary,
+          completing my degree in May 2024. I specialize in compiler technology,
+          game engine technology, and computer architecture.
+        </p>
+        <br />
+        <p>
+          I&apos;m currently working on{" "}
+          <Link href="https://crp-docs.vercel.app/" target="_blank">
+            create ros project
+          </Link>{" "}
+          to improve the way we build software for robots.
         </p>
         <br />
         <p>
           I&apos;m currently searching for my next opportunity. If you&apos;re
-          interested in working with me, please <Link href="/#contact">reach out</Link>!
+          interested in working with me, please{" "}
+          <Link href="/#contact">reach out</Link>!
         </p>
         <br />
         <p>
-          In the meantime, I&apos;m working on an education platform with{" "}
-          <Link href="">Elements Education</Link> to improve the way we learn,
-          and{" "}
-          <Link href="https://crp-docs.vercel.app/" target="_blank">
-            create ros project
-          </Link>{" "}
-          to improve the way we build robotics software.
+          Aside from my work life, I love to play music, read, learn about
+          ancient history, run local video game tournaments, cook, and jog.
         </p>
       </main>
     </>
