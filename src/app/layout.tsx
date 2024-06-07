2e11f9a089bde5e4e692059b085355414ddd6ad5
Beau McCartney <mccartney.beausl@gmail.com>
2024-06-07 15:27:19 -0600

    remove unneeded styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,13 +22,13 @@ export default function RootLayout(props: { children: React.ReactNode }) {
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
       <body className=" text-neutral-700 antialiased">
-        <div className="container mx-auto grid min-h-screen max-w-xl grid-rows-[auto,1fr] gap-2">
-          <header className="flex flex-wrap items-center justify-between border-b-2 p-4 text-xl">
+        <div className="container mx-auto grid max-w-xl gap-1">
+          <header className="flex justify-between border-b-2 p-4 text-xl">
             <div className="font-bold">
               {/* TODO: link to new resume */}
               <TopLink href="https://utfs.io/f/27ac9f65-0d4c-4076-817c-a63b6032cb62-eyyx7p.pdf" name="Resume" />
             </div>
-            <div className="flex flex-row flex-wrap items-center gap-10">
+            <div className="flex gap-10">
               <TopLink href="https://github.com/BeauSLM" name="GitHub" />
               <TopLink
                 href="https://www.linkedin.com/in/beau-mccartney-247b0820a/"
