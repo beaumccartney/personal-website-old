ea459c56bc5b5391cb547bdb2b9db166ae11a724
Beau McCartney <mccartney.beausl@gmail.com>
2024-06-08 23:24:40 -0600

    put form things in divs

--- src/app/web3form.tsx
+++ src/app/web3form.tsx
@@ -6,13 +6,25 @@ import submitForm from "./submitForm";
 export default function Web3Form() {
   const [result, formAction] = useFormState(submitForm, null);
 
+  // TODO: factor out border styles from the project cards to these too
+  // ...@apply?
   return (
     <>
       <form action={formAction}>
-        <input placeholder="Name" type="text" name="name" required />
-        <input placeholder="Email" type="email" name="email" required />
-        <textarea placeholder="Message" name="message" required></textarea>
-        <button type="submit">Submit</button>
+        <div className="section-items">
+          <div className="border border-neutral-200 placeholder:text-neutral-500">
+            <input placeholder="Name" type="text" name="name" required />
+          </div>
+          <div className="border border-neutral-200 placeholder:text-neutral-500">
+            <input placeholder="Email" type="email" name="email" required />
+          </div>
+          <div className="border border-neutral-200 placeholder:text-neutral-500">
+            <textarea placeholder="Message" name="message" required></textarea>
+          </div>
+          <div>
+            <button type="submit">Submit</button>
+          </div>
+        </div>
       </form>
       <span>{result?.message}</span>
     </>
